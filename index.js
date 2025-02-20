680d9bc22f6c1b27c5e373a750de441da7aac597
Facundo Sura <facundomesura@gmail.com>
2025-02-20 12:55:47 -0300

    deploy

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const axios = require("axios");
+require("dotenv").config();
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
+conn
+  .sync({ force: process.env.NODE_ENV !== "production" })
+  .then(() => {
+    server.listen(port, "0.0.0.0", () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error.message);
+    console.error("Stack trace:", error.stack);
+    process.exit(1); 
+  });
